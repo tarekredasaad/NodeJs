387236c061cab8172eae4a01f8bf8852cc605747
Tarek RedaSaad <redatarek179@gmail.com>
2023-04-05 14:56:13 +0200

    nodejs

--- new.js
+++ new.js
@@ -2,6 +2,9 @@ var newAdd = require('./index.js');
 
 console.log(5);
 console.log(newAdd.first(9,6));
+console.log(newAdd.first("k",6));
 console.log(newAdd.second(10,4));
+console.log(newAdd.second("p",4));
 console.log(newAdd.third(10,2));
 console.log(newAdd.five("tariq",2024));
+console.log(newAdd.five("tariq",1998));
